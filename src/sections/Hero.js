6aea99b80bf1102b6445df2e6cc61d1dba6edee5
Benton Parker <bentonparker@Bentons-Air.lan>
2024-07-15 10:59:34 -0500

    Updated Hero section layout

--- src/sections/Hero.js
+++ src/sections/Hero.js
@@ -2,21 +2,19 @@ import { useState } from 'react';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import { useSession } from 'next-auth/react';
 import Link from 'next/link';
-import { useTranslation } from "react-i18next";
 
 const Hero = () => {
   const { status: sessionStatus } = useSession();
   const [showMenu, setMenuVisibility] = useState(false);
 
   const toggleMenu = () => setMenuVisibility(!showMenu);
-  const { t } = useTranslation();
 
   return (
     <div className="w-full py-10">
       <div className="relative flex flex-col px-10 mx-auto space-y-5 md:w-3/4">
         <header className="flex items-center justify-between space-x-3">
           <Link href="/" className="text-2xl font-bold">
-            Nextacular
+            Benton Parker
           </Link>
           <button className="md:hidden" onClick={toggleMenu}>
             {!showMenu ? (
@@ -34,9 +32,11 @@ const Hero = () => {
             ].join(' ')}
           >
             <nav className="flex flex-col w-full space-x-0 space-y-3 text-center md:space-y-0 md:space-x-3 md:flex-row">
-              <a className="px-5 py-2 rounded hover:bg-gray-100">{t("common.label.guides")}</a>
-              <a className="px-5 py-2 rounded hover:bg-gray-100">{t("common.label.pricing")}</a>
-              <a className="px-5 py-2 rounded hover:bg-gray-100">{t("common.label.blog")}</a>
+              <a className="px-5 py-2 rounded hover:bg-gray-100">About</a>
+              <a className="px-5 py-2 rounded hover:bg-gray-100">Schedule</a>
+              <a className="px-5 py-2 rounded hover:bg-gray-100">Media</a>
+              {/* <a className="px-5 py-2 rounded hover:bg-gray-100">Store</a> */}
+              <a className="px-5 py-2 rounded hover:bg-gray-100">Contact</a>
             </nav>
             <Link
               href={
@@ -49,20 +49,17 @@ const Hero = () => {
           </div>
         </header>
         <div className="flex flex-col items-center justify-center pt-10 mx-auto md:w-3/5">
-          <h1 className="text-6xl font-extrabold text-center">
-            <span className="block">Build SaaS platforms</span>
-            <span className="block text-blue-600">like never before</span>
+          <h1 className="text-5xl font-extrabold text-center">
+            <span className="block text-blue-600">Blues and Soul</span>
+            <span className="block">from Memphis, TN</span>
           </h1>
           <p className="mt-5 text-center text-gray-600">
-            Quickly build landing pages that will help you get results fast
+            Singer, Songwriter, Guitar, and Bass
           </p>
         </div>
         <div className="flex items-center justify-center space-x-5">
           <a className="px-10 py-3 text-center text-white bg-blue-600 rounded shadow hover:bg-blue-500">
-            Get Started
-          </a>
-          <a className="px-10 py-3 text-center text-blue-600 rounded shadow hover:bg-blue-50">
-            Live Demo
+            See some Video! (link to Youtube?)
           </a>
         </div>
       </div>
