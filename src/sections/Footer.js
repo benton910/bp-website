ca672c47274ed2098108c860e88551bea516e616
Benton Parker <bentonparker@Bentons-MacBook-Air.local>
2024-07-19 01:15:38 -0500

    Finished footer logos

--- src/sections/Footer.js
+++ src/sections/Footer.js
@@ -1,6 +1,9 @@
 import Image from 'next/image';
-import instaImg from '../../public/images/PROMOcopy/SOCIAL/IMG_3642_LE_auto_x2.png';
-import fbImage from '../../public/images/PROMOcopy/SOCIAL/IMG_5378_LE_auto_x2.png';
+import instaIcon from '../../public/images/PROMOcopy/SOCIAL/IMG_3642_LE_auto_x2.png';
+import fbIcon from '../../public/images/PROMOcopy/SOCIAL/IMG_5378_LE_auto_x2.png';
+import youtubeIcon from '../../public/images/PROMOcopy/SOCIAL/Youtube_logo_LE_auto_x2.png';
+import venmoIcon from '../../public/images/PROMOcopy/SOCIAL/IMG_8140_LE_auto_x2.png';
+import cashappIcon from '../../public/images/PROMOcopy/SOCIAL/IMG_4102_LE_auto_x2.png';
 
 const Footer = () => {
   return (
@@ -9,8 +12,8 @@ const Footer = () => {
         <footer className="flex flex-col items-center justify-center py-5 space-x-0 space-y-3 border-b md:flex-row md:space-y-0 md:space-x-3">
           <a href="https://www.instagram.com/bentonparkermusic" target="_blank" className="px-5 py-2 text-xl rounded hover:underline">
             <Image
-              src={instaImg}
-              alt="Picture of the main text logo"
+              src={instaIcon}
+              alt="Instagram icon"
               width={50}
               height={50}
             // blurDataURL="data:..." automatically provided
@@ -19,17 +22,44 @@ const Footer = () => {
           </a>
           <a href="https://www.facebook.com/benton.parker.92/" target="_blank" className="px-5 py-2 text-xl rounded hover:underline">
             <Image
-              src={fbImage}
-              alt="Picture of the main text logo"
+              src={fbIcon}
+              alt="Facebook icon"
               width={92}
               height={92}
             // blurDataURL="data:..." automatically provided
             // placeholder="blur" // Optional blur-up while loading
             />
           </a>
-          <a className="px-5 py-2 text-xl rounded hover:underline">Youtube</a>
-          <a className="px-5 py-2 text-xl rounded hover:underline">Venmo</a>
-          <a className="px-5 py-2 text-xl rounded hover:underline">CashApp</a>
+          <a href="https://www.youtube.com/@bentonparkermusic/videos" target="_blank" className="px-5 py-2 text-xl rounded hover:underline">
+            <Image
+              src={youtubeIcon}
+              alt="Youtube icon"
+              width={50}
+              height={50}
+            // blurDataURL="data:..." automatically provided
+            // placeholder="blur" // Optional blur-up while loading
+            />
+          </a>
+          <a href="https://account.venmo.com/u/bentonparkermusic" target="_blank" className="px-5 py-2 text-xl rounded hover:underline">
+            <Image
+              src={venmoIcon}
+              alt="Venmo icon"
+              width={50}
+              height={50}
+            // blurDataURL="data:..." automatically provided
+            // placeholder="blur" // Optional blur-up while loading
+            />
+          </a>
+          <a href="https://cash.app/$bentonparkermusic" target="_blank" className="px-5 py-2 text-xl rounded hover:underline">
+            <Image
+              src={cashappIcon}
+              alt="CashApp icon"
+              width={50}
+              height={50}
+            // blurDataURL="data:..." automatically provided
+            // placeholder="blur" // Optional blur-up while loading
+            />
+          </a>
         </footer>
         <p className="py-5 text-center text-gray-400">
           &copy; Nextacular. All rights reserved {new Date().getFullYear()}.
