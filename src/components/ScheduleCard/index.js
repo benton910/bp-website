1f0cb534f8cb7285647f7d903ab3d331b24af5d7
Benton Parker <bentonparker@Bentons-Air.lan>
2024-08-09 13:22:10 -0500

    Updated ScheduleCard scenarios and layouts

--- src/components/ScheduleCard/index.js
+++ src/components/ScheduleCard/index.js
@@ -9,19 +9,6 @@
 // venueUrl           String
 
 
-//Need some helper functions to do a few things:
-// 2) Helper function to determine How to display based on bpRole
-//    - if bpRole == SOLO , then the personnel should contain the name of the person that bp is playing with,
-//      and the gig title should say something like 'w/ <personnel> @ <gigVenue>'
-//    - if bpRole == DUO , then the personnel should contain the name of the person that bp is playing with,
-//      and the gig title should say something like 'Duo w/ <personnel> @ <gigVenue>'
-//    - if bpRole == SIDEMAN, BASS | GUITAR , then the personnel should contain the name of the person that bp is playing with,
-//      and the gig title should say something like '<personnel, aka bandleader, ex. Tony Holiday> @ <gigVenue>', and in the
-//      event description there should be information containing the instrument BP will be playing
-//    - if bpRole == FRONTMAN, FULLBAND, then the personnel should contain the names of the people in the RR's that night,
-//      and the gig title should say something like 'BP & The Royal Reds @ <gigVenue>' (Applies for 3 piece & up)
-
-
 const formatDate = (date) => {
   const newDate = new Date(date);
   return new Intl.DateTimeFormat('en-US').format(newDate);
@@ -33,17 +20,46 @@ const formatDateTimeRange = (dateTime1, dateTime2) => {
   return newDate1.toLocaleTimeString('en-US', { timeStyle: 'short' }) + ' - ' + newDate2.toLocaleTimeString('en-US', { timeStyle: 'short' });
 };
 
+
+// Let description == instrument you're playing OR the people you're playing with, 'ft. ___, ___ '
+// Personnel = people in Royal Reds if bpRole=FRONTMAN, ELSE it's the name of person bp is backing if bpRole=SIDEMAN
+const generateDisplayInfo = (scProps) => {
+  if (scProps.bpRole === 'SOLO') {
+    return {
+      title: 'SOLO @ ' + scProps.gigVenue,
+      description: null
+    }
+  } else if (scProps.bpRole === 'DUO') {
+    return {
+      title: 'DUO w/ ' + scProps.personnel + ' @ ' + scProps.gigVenue,
+      description: null
+    }
+  } else if (scProps.bpRole === 'FRONTMAN') {
+    return {
+      title: 'BP & The Royal Reds @ ' + scProps.gigVenue,
+      description: 'ft. ' + scProps.personnel
+    }
+  }
+  else if (scProps.bpRole === 'SIDEMAN') {
+    return {
+      title: 'w/ ' + scProps.personnel + ' @ ' + scProps.gigVenue,
+      description: null
+    }
+  }
+}
+
 const ScheduleCard = (scheduleCardProps) => {
+  const cardDisplayInfo = generateDisplayInfo(scheduleCardProps);
   return (
     <div className="w-full mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl m-5 hover:scale-105 transform transition duration-100">
       <div className="md:flex">
         <div className="p-8">
-          <div className="uppercase tracking-wide text-3xl text-blue-600 font-semibold">@ {scheduleCardProps.gigVenue}</div>
+          <div className="uppercase tracking-wide text-2xl text-blue-600 font-semibold">{cardDisplayInfo.title}</div>
           <p suppressHydrationWarning className="block mt-4 text-lg leading-tight font-medium text-black">{formatDate(scheduleCardProps.startTime)}</p>
           <p suppressHydrationWarning className="block mt-1 text-md leading-tight font-medium text-black">{formatDateTimeRange(scheduleCardProps.startTime, scheduleCardProps.endTime)}</p>
           <p className="block mt-1 text-md leading-tight font-medium text-black">{scheduleCardProps.cityState}</p>
-          {scheduleCardProps.personnel ? <p className="mt-2 text-gray-500">ft. {scheduleCardProps.personnel}</p> : null}
-          {scheduleCardProps.venueUrl ? <a href={scheduleCardProps.venueUrl} target="_blank" className="mt-3 text-blue-500">More info</a> : null}
+          <p className="mt-2 text-gray-500">{cardDisplayInfo.description}</p>
+          {scheduleCardProps.venueUrl ? <a href={scheduleCardProps.venueUrl} target="_blank" className="mt-5 text-blue-500">More info</a> : null}
         </div>
       </div>
     </div>
