a7b338b3b650aa1532bf28f05d7d887f396f8d6a
Benton Parker <bentonparker@Bentons-Air.lan>
2024-08-09 12:28:40 -0500

    Suppressing hydration warning for time formatting of Schedulecard info

--- src/components/ScheduleCard/index.js
+++ src/components/ScheduleCard/index.js
@@ -23,13 +23,14 @@
 
 
 const formatDate = (date) => {
-  console.log(new Intl.DateTimeFormat('en-US').format(date));
-  return new Intl.DateTimeFormat('en-US').format(date);
+  const newDate = new Date(date);
+  return new Intl.DateTimeFormat('en-US').format(newDate);
 };
 
 const formatDateTimeRange = (dateTime1, dateTime2) => {
-  console.log(dateTime1.toLocaleTimeString('en-US', { timeStyle: 'short' }) + ' - ' + dateTime2.toLocaleTimeString('en-US', { timeStyle: 'short' }));
-  return dateTime1.toLocaleTimeString('en-US', { timeStyle: 'short' }) + ' - ' + dateTime2.toLocaleTimeString('en-US', { timeStyle: 'short' });
+  const newDate1 = new Date(dateTime1);
+  const newDate2 = new Date(dateTime2);
+  return newDate1.toLocaleTimeString('en-US', { timeStyle: 'short' }) + ' - ' + newDate2.toLocaleTimeString('en-US', { timeStyle: 'short' });
 };
 
 const ScheduleCard = (scheduleCardProps) => {
@@ -38,8 +39,8 @@ const ScheduleCard = (scheduleCardProps) => {
       <div className="md:flex">
         <div className="p-8">
           <div className="uppercase tracking-wide text-3xl text-blue-600 font-semibold">@ {scheduleCardProps.gigVenue}</div>
-          <p className="block mt-4 text-lg leading-tight font-medium text-black">{formatDate(new Date(scheduleCardProps.startTime))}</p>
-          <p className="block mt-1 text-md leading-tight font-medium text-black">{formatDateTimeRange(new Date(scheduleCardProps.startTime), new Date(scheduleCardProps.endTime))}</p>
+          <p suppressHydrationWarning className="block mt-4 text-lg leading-tight font-medium text-black">{formatDate(scheduleCardProps.startTime)}</p>
+          <p suppressHydrationWarning className="block mt-1 text-md leading-tight font-medium text-black">{formatDateTimeRange(scheduleCardProps.startTime, scheduleCardProps.endTime)}</p>
           <p className="block mt-1 text-md leading-tight font-medium text-black">{scheduleCardProps.cityState}</p>
           {scheduleCardProps.personnel ? <p className="mt-2 text-gray-500">ft. {scheduleCardProps.personnel}</p> : null}
           {scheduleCardProps.venueUrl ? <a href={scheduleCardProps.venueUrl} target="_blank" className="mt-3 text-blue-500">More info</a> : null}
