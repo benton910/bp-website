7eaacb92f475a12c8af2b95fb5e03c8fd692067d
Benton Parker <bentonparker@Bentons-Air.lan>
2024-09-20 03:07:31 -0600

    Adding structured data for shows

--- src/components/ScheduleCard/index.js
+++ src/components/ScheduleCard/index.js
@@ -1,7 +1,12 @@
+import { generateEventStructuredData } from "@/lib/utils/structured-data-generator";
+import { generateDisplayInfo } from "@/lib/utils/display-info-generator";
+import { formatDate } from "@/lib/utils/date-formatter";
+import { formatDateTimeRange } from "@/lib/utils/date-formatter";
+
 // id                 String @id @default (cuid())
 // createdAt          DateTime ? @default (now())
-// startTime  DateTime
-// endTime    DateTime
+// startTime          DateTime
+// endTime            DateTime
 // gigVenue           String
 // cityState          String
 // personnel          String
@@ -9,49 +14,15 @@
 // venueUrl           String
 
 
-const formatDate = (date) => {
-  const newDate = new Date(date);
-  return new Intl.DateTimeFormat('en-US').format(newDate);
-};
-
-const formatDateTimeRange = (dateTime1, dateTime2) => {
-  const newDate1 = new Date(dateTime1);
-  const newDate2 = new Date(dateTime2);
-  return newDate1.toLocaleTimeString('en-US', { timeStyle: 'short' }) + ' - ' + newDate2.toLocaleTimeString('en-US', { timeStyle: 'short' });
-};
-
-
-// Let description == instrument you're playing OR the people you're playing with, 'ft. ___, ___ '
-// Personnel = people in Royal Reds if bpRole=FRONTMAN, ELSE it's the name of person bp is backing if bpRole=SIDEMAN
-const generateDisplayInfo = (scProps) => {
-  if (scProps.bpRole === 'SOLO') {
-    return {
-      title: 'SOLO @ ' + scProps.gigVenue,
-      description: null
-    }
-  } else if (scProps.bpRole === 'DUO') {
-    return {
-      title: 'DUO w/ ' + scProps.personnel + ' @ ' + scProps.gigVenue,
-      description: null
-    }
-  } else if (scProps.bpRole === 'FRONTMAN') {
-    return {
-      title: 'BP & The Royal Reds @ ' + scProps.gigVenue,
-      description: 'ft. ' + scProps.personnel
-    }
-  }
-  else if (scProps.bpRole === 'SIDEMAN') {
-    return {
-      title: 'w/ ' + scProps.personnel + ' @ ' + scProps.gigVenue,
-      description: null
-    }
-  }
-}
-
 const ScheduleCard = (scheduleCardProps) => {
   const cardDisplayInfo = generateDisplayInfo(scheduleCardProps);
+  const jsonLd = generateEventStructuredData(scheduleCardProps, cardDisplayInfo);
   return (
     <div className="w-full mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl m-5 hover:scale-105 transform transition duration-100">
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      />
       <div className="md:flex">
         <div className="p-8">
           <div className="uppercase tracking-wide text-2xl text-blue-600 font-semibold">{cardDisplayInfo.title}</div>
--- src/lib/utils/structured-data-generator.js
+++ src/lib/utils/structured-data-generator.js
@@ -1,56 +1,66 @@
+import { generateDisplayInfo } from "./display-info-generator";
+
 //@type=Event required properties are:
 //  location
 //  location.address
 //  location.url
 //  name
 //  startDate (in ISO-8601 format)
-export const generateEventStructuredData = (allShows) => {
-    let eventsStructureData = allShows.map((show) => {
-        return {
-            "@context": "https://schema.org",
-            "@type": "Event",
-            "name": "",
-            "startDate": "2025-07-21T19:00-05:00",
-            "endDate": "2025-07-21T23:00-05:00",
-            "eventAttendanceMode": "https://schema.org/OfflineEventAttendanceMode",
-            "eventStatus": "https://schema.org/EventScheduled",
-            "location": {
-                "@type": "Place",
-                "name": "Snickerpark Stadium",
-                "address": {
-                    "@type": "PostalAddress",
-                    "streetAddress": "100 West Snickerpark Dr",
-                    "addressLocality": "Snickertown",
-                    "postalCode": "19019",
-                    "addressRegion": "PA",
-                    "addressCountry": "US"
-                }
-            },
-            "image": [
-                "https://example.com/photos/1x1/photo.jpg",
-                "https://example.com/photos/4x3/photo.jpg",
-                "https://example.com/photos/16x9/photo.jpg"
-            ],
-            "description": "The Adventures of Kira and Morrison is coming to Snickertown in a can't miss performance.",
-            "offers": {
-                "@type": "Offer",
-                "url": "https://www.example.com/event_offer/12345_202403180430",
-                "price": 30,
-                "priceCurrency": "USD",
-                "availability": "https://schema.org/InStock",
-                "validFrom": "2024-05-21T12:00"
-            },
-            "performer": {
-                "@type": "PerformingGroup",
-                "name": "Kira and Morrison"
-            },
-            "organizer": {
-                "@type": "Organization",
-                "name": "Kira and Morrison Music",
-                "url": "https://kiraandmorrisonmusic.com"
-            }
-        }
-    })
-    return eventsStructureData;
+
+//allShows contain:
+//   id = { show.id }
+//   createdAt = { show.createdAt }
+//   startTime = { show.showDateStartTime }
+//   endTime = { show.showDateEndTime }
+//   gigVenue = { show.gigVenue }
+//   cityState = { show.cityState }
+//   personnel = { show.personnel }
+//   bpRole = { show.bpRole }
+//   venueUrl = { show.venueUrl }
+
+export const generateEventStructuredData = (showProps, showDisplayInfo) => {
+  return {
+    "@context": "https://schema.org",
+    "@type": "Event",
+    "name": showDisplayInfo.title,
+    "startDate": showProps.startTime,
+    "endDate": showProps.endTime,
+    "eventStatus": "https://schema.org/EventScheduled",
+    "location": {
+      "@type": "Place",
+      "name": showProps.gigVenue,
+      "address": {
+        "@type": "PostalAddress",
+        "streetAddress": "100 West Snickerpark Dr",
+        "addressLocality": "Snickertown",
+        "postalCode": "19019",
+        "addressRegion": "PA",
+        "addressCountry": "US"
+      },
+      "url": showProps.venueUrl
+    },
+    // "image": [
+    //   "https://example.com/photos/1x1/photo.jpg",
+    //   "https://example.com/photos/4x3/photo.jpg",
+    //   "https://example.com/photos/16x9/photo.jpg"
+    // ],
+    // "offers": {
+    //   "@type": "Offer",
+    //   "url": "https://www.example.com/event_offer/12345_202403180430",
+    //   "price": 30,
+    //   "priceCurrency": "USD",
+    //   "availability": "https://schema.org/InStock",
+    //   "validFrom": "2024-05-21T12:00"
+    // },
+    // "performer": {
+    //   "@type": "PerformingGroup",
+    //   "name": "Kira and Morrison"
+    // },
+    // "organizer": {
+    //   "@type": "Organization",
+    //   "name": "Kira and Morrison Music",
+    //   "url": "https://kiraandmorrisonmusic.com"
+    // }
+  }
 };
 
